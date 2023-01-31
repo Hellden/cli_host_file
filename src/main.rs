cd59a12de93209fec54283dac6e8a8f94bd59b9e
Hellden GHT <rchapotin@ch.fr>
2023-01-31 17:02:55 +0100

    Add check line hosts

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![allow(unused_variables)]
 use clap::{Args, Parser, Subcommand};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 fn about() {
     print!("##### Welcome cli-host_file #####");
@@ -25,19 +29,20 @@ struct Cli {
 enum Commands {
     /// Add row in host file
     Add(Add),
-    Delete{
+    Delete {
         /// Name row to delete
-        name:String,
-        ip:String
-    }
+        ip: String,
+        name: String,
+    },
 }
 
 #[derive(Args)]
 struct Add {
+    ip: String,
     name: String,
-    ip: String
 }
 
+const FILEPATH: &str = "/etc/hosts";
 
 fn main() {
     about();
@@ -47,10 +52,47 @@ fn main() {
     // matches just as you would the top level cmd
     match cli.command {
         Commands::Add(payload) => {
-            println!("'myapp add' was used, name is: {} {}", payload.name, payload.ip)
-        },
+            if !check_lines(payload) {
+                add_line(payload);
+            }
+        }
         Commands::Delete { name, ip } => {
             println!("It's live {} {}", name, ip)
         }
     }
+}
+
+fn check_lines(payload: Add) -> bool {
+    // Open host file
+    let file = match File::open(FILEPATH) {
+        Ok(file) => file,
+        Err(e) => {
+            println!("Impossible d'ouvrir le fichier hosts: {}", e);
+            return false;
+        }
+    };
+
+    let reader = BufReader::new(file);
+
+    for (line_number, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        if line.contains(&payload.name) {
+            println!(
+                "Une entrée existe déja pour : {} à la ligne {}",
+                payload.name, line_number
+            );
+            return true;
+        }
+    }
+    return false;
+}
+
+fn add_line(payload: Add) {
+    let file = match File::open(FILEPATH) {
+        Ok(file) => file,
+        Err(e) => {
+            println!("Impossible d'ouvrir le fichier hosts: {}", e);
+            return;
+        }
+    };
 }
